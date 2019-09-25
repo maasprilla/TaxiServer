60f884fd3285f5d0cbe472b7e6b0c1aacc7df5ed
Miguel Angel Asprilla <maasprilla35@misena.edu.co>
2019-09-25 13:14:09 -0500

    commit

--- utils/pass.js
+++ utils/pass.js
@@ -1,1 +1,27 @@
-(function(){var t,n,r;t=require("crypto"),r=128,n=12e3,exports.hash=function(l,i,u){var e;if(3===arguments.length)try{t.pbkdf2(l,i,n,r,function(t,n){t?u(t,null):(n=n.toString("base64"),u(null,n))})}catch(o){e=o,u(e.toString(),null)}else u=i,t.randomBytes(r,function(i,o){if(i)return u(i);o=o.toString("base64");try{t.pbkdf2(l,o,n,r,function(t,n){return t?u(t):(n=n.toString("base64"),void u(null,o,n))})}catch(a){e=a,u(e.toString(),null,null)}})}}).call(this);
+(function() {
+    var t, n, r;
+    t = require("crypto"), r = 128, n = 12e3, exports.hash = function(l, i, u) {
+        var e;
+        if (3 === arguments.length) try {
+            console.log('hash 1');
+            t.pbkdf2(l, i, n, r, function(t, n) {
+                t ? u(t, null) : (n = n.toString("base64"), u(null, n))
+            })
+        } catch (o) {
+            console.log('hash 2');
+            e = o, u(e.toString(), null)
+        } else u = i, t.randomBytes(r, function(i, o) {
+            if (i) return u(i);
+            o = o.toString("base64");
+            try {
+                console.log('hash 3');
+                t.pbkdf2(l, o, n, r, function(t, n) {
+                    return t ? u(t) : (n = n.toString("base64"), void u(null, o, n))
+                })
+            } catch (a) {
+                console.log('hash 4');
+                e = a, u(e.toString(), null, null)
+            }
+        })
+    }
+}).call(this);
