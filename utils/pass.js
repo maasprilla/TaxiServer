fe949efe97576ffa0226a9cf7731aa6d5db859a9
Miguel Angel Asprilla <maasprilla35@misena.edu.co>
2019-09-25 14:25:04 -0500

    commit

--- utils/pass.js
+++ utils/pass.js
@@ -1,6 +1,6 @@
-(function() {
+(function () {
     var t, n, r;
-    t = require("crypto"), r = 128, n = 12e3, exports.hash = function(l, i, u) {
+    t = require("crypto"), r = 128, n = 12e3, exports.hash = function (l, i, u) {
         var e;
         if (3 === arguments.length) try {
             console.log('hash 1');
@@ -8,12 +8,12 @@
             console.log(i);
             console.log(n);
             console.log(r);
-           
-            
+
+
             // crypto.pbkdf2( value, salt, iterations, hashBytes, digest, (err, key) => {
 
 
-            t.pbkdf2(l, i, n, r,'base64', function(t, n) {
+            t.pbkdf2(l, i, n, r, 'sha512', function (t, n) {
                 console.log('result hast');
                 console.log(n);
                 console.log(JSON.stringify(n));
@@ -23,12 +23,12 @@
             console.log('hash 2');
             console.log(o);
             e = o, u(e.toString(), null)
-        } else u = i, t.randomBytes(r, function(i, o) {
+        } else u = i, t.randomBytes(r, function (i, o) {
             if (i) return u(i);
             o = o.toString("base64");
             try {
                 console.log('hash 3');
-                t.pbkdf2(l, o, n, r, function(t, n) {
+                t.pbkdf2(l, o, n, r, function (t, n) {
                     return t ? u(t) : (n = n.toString("base64"), void u(null, o, n))
                 })
             } catch (a) {
